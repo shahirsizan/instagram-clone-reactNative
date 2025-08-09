18822d2944287fc51bf05ae6e7d9eab289669ce0
shahirsizan <shahir.sizan18@gmail.com>
2025-08-09 23:09:28 +0600

    Integrated Convex(BaaS) schemas to model tables in database.

---
+++ convex/schema.js
@@ -0,0 +1,69 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+	users: defineTable({
+		username: v.string(),
+		fullname: v.string(),
+		email: v.string(),
+		bio: v.optional(v.string()),
+		image: v.string(),
+		followers: v.number(),
+		following: v.number(),
+		clerkId: v.string(),
+	}).index("by_clerk_Id", ["clerkId"]),
+
+	posts: defineTable({
+		userId: v.id("users"),
+		imageUrl: v.string(),
+		storageId: v.id("_storage"), // will be needed when we want to delete a post
+		caption: v.optional(v.string()),
+		likes: v.number(),
+		comments: v.number(),
+	}).index("by_user", ["userId"]),
+
+	likes: defineTable({
+		userId: v.id("users"),
+		postId: v.id("posts"),
+	})
+		.index("by_post", ["postId"])
+		.index("by_user_and_post", ["userId", "postId"]),
+
+	comments: defineTable({
+		userId: v.id("users"),
+		postId: v.id("posts"),
+		content: v.string(),
+	}).index("by_post", ["postId"]),
+
+	follows: defineTable({
+		followerId: v.id("users"),
+		followingId: v.id("users"),
+	})
+		.index("by_follower", ["followerId"])
+		.index("by_following", ["followingId"])
+		.index("by_both", ["followerId", "followingId"]),
+
+	notifications: defineTable({
+		senderId: v.id("users"),
+		receiverId: v.id("users"),
+		type: v.union(
+			v.literal("like"),
+			v.literal("comment"),
+			v.literal("follow")
+		),
+		postId: v.optional(v.id("posts")),
+		commentId: v.optional(v.id("comments")),
+		// Above two attributes are `optional` because a `follow` notification won't have any Id
+		// Only a `like or a `comment` Ids of the post being manipulated
+	})
+		.index("by_receiver", ["receiverId"])
+		.index("by_post", ["postId"]),
+
+	bookmarks: defineTable({
+		userId: v.id("users"),
+		postId: v.id("posts"),
+	})
+		.index("by_user", ["userId"])
+		.index("by_post", ["postId"])
+		.index("by_user_and_post", ["userId", "postId"]),
+});
