e88a2c6b602b7f5d6d11ccecc29b1c80a99d6d0b
shahirsizan <shahir.sizan18@gmail.com>
2025-08-14 13:08:28 +0600

    Implemented Convex/posts.js/getFeedPosts-API to fetch all posts. Each post will have its post-content, user-info & like-bookmark-status. Now time to implement the home-tab-UI to actually fetch those data.

--- convex/posts.js
+++ convex/posts.js
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
+import { getAuthenticatedUser } from "./users";
 
 // 3 steps to upload file to convex storage:
 //     1. Generate an upload URL using a mutation that calls `storage.generateUploadUrl()`.
@@ -19,20 +20,7 @@ export const createPost = mutation({
 		storageId: v.id("_storage"),
 	},
 	handler: async (ctx, args) => {
-		const identity = await ctx.auth.getUserIdentity();
-		if (!identity) {
-			throw new Error("Unauthorized user!");
-		}
-
-		// fetch current user info from convex db
-		const currentUser = await ctx.db
-			.query("users")
-			.withIndex("by_clerk_id", (q) => q.eq("clerkId", identity.subject))
-			.first();
-
-		if (!currentUser) {
-			throw new Error("User not found!");
-		}
+		const currentUser = getAuthenticatedUser(ctx);
 
 		const imageUrl = await ctx.storage.getUrl(args.storageId);
 
@@ -54,5 +42,63 @@ export const createPost = mutation({
 		await ctx.db.patch(currentUser._id, { posts: currentUser.posts + 1 });
 
 		return postId;
+	},
+});
+
+export const getFeedPosts = query({
+	args: {},
+	handler: async (ctx, args) => {
+		const currentUser = await getAuthenticatedUser(ctx);
+
+		// get all posts
+		const posts = await ctx.db.query("posts").order("desc").collect();
+
+		if (posts.length === 0) {
+			return [];
+		}
+
+		// incorporate userdata and interaction status to the posts
+		const postsWithInfo = await Promise.all(
+			posts.map(async (post) => {
+				// fetch author info
+				const postAuthor = await ctx.db.get("posts", post.userId);
+
+				// fetch if liked by me
+				const isLiked = await ctx.db
+					.query("likes")
+					.withIndex("by_user_and_post", (q) => {
+						return q
+							.eq("postId", post._id)
+							.eq("userId", currentUser._id);
+					})
+					.first();
+
+				// fetch if bookmarked
+				const isBookmarked = await ctx.db
+					.query("bookmarks")
+					.withIndex("by_user_and_post", (q) => {
+						return q
+							.eq("postId", post._id)
+							.eq("userId", currentUser._id);
+					})
+					.first();
+
+				return {
+					...post,
+					author: {
+						_id: postAuthor._id,
+						username: postAuthor.username,
+						image: postAuthor.image,
+					},
+					// The `!!` is a quick way to cast a value to a boolean.
+					// If isLiked/isBookmarked is an object, a string, or any "truthy" value,
+					// !!isLiked or !!isBookmarked will be `true`. Else false.
+					isLiked: !!isLiked,
+					isBookmarked: !!isBookmarked,
+				};
+			})
+		);
+
+		return postsWithInfo;
 	},
 });
--- convex/users.js
+++ convex/users.js
@@ -3,6 +3,27 @@
 import { v } from "convex/values";
 import { mutation } from "./_generated/server";
 
+export const getAuthenticatedUser = async (ctx) => {
+	const identity = await ctx.auth.getUserIdentity();
+
+	if (!identity) {
+		throw new Error("Unauthorized!");
+	}
+
+	const currentUser = await ctx.db
+		.query("users")
+		.withIndex("by_clerk_id", (q) => {
+			return q.eq("clerkId", identity.subject);
+		})
+		.first();
+
+	if (!currentUser) {
+		throw new Error("User not found");
+	}
+
+	return currentUser;
+};
+
 export const createUser = mutation({
 	args: {
 		username: v.string(),
