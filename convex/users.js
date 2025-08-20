5ed9488a26770f1bcba961679ba6bcb5836668fa
shahirsizan <shahir.sizan18@gmail.com>
2025-08-20 19:02:42 +0600

    Implemented getUserProfile, isFollowing & toggleFollow Convex-API in backend. Frontend UI yet to be implemented.

--- convex/users.js
+++ convex/users.js
@@ -87,3 +87,101 @@ export const updateProfile = mutation({
 		});
 	},
 });
+
+export const getUserProfile = query({
+	args: { id: v.id("users") },
+	handler: async (ctx, args) => {
+		const user = await ctx.db.get(args.id);
+		if (!user) {
+			throw new Error("User not found");
+		}
+
+		return user;
+	},
+});
+
+export const isFollowing = query({
+	args: { followingId: v.id("users") },
+	handler: async (ctx, args) => {
+		const currentUser = await getAuthenticatedUser(ctx);
+
+		const isFollowing = await ctx.db
+			.query("follows")
+			.withIndex("by_both", (q) =>
+				q
+					.eq("followerId", currentUser._id)
+					.eq("followingId", args.followingId)
+			)
+			.first();
+
+		return !!isFollowing;
+	},
+});
+
+export const toggleFollow = mutation({
+	args: { followingId: v.id("users") },
+	handler: async (ctx, args) => {
+		const currentUser = await getAuthenticatedUser(ctx);
+
+		const isAlreadyFollowing = await ctx.db
+			.query("follows")
+			.withIndex("by_both", (q) =>
+				q
+					.eq("followerId", currentUser._id)
+					.eq("followingId", args.followingId)
+			)
+			.first();
+
+		if (isAlreadyFollowing) {
+			// unfollow
+			await ctx.db.delete(isAlreadyFollowing._id);
+
+			await updateFollowCounts(
+				ctx,
+				currentUser._id,
+				args.followingId,
+				"unfollow"
+			);
+		} else {
+			// follow
+			await ctx.db.insert("follows", {
+				followerId: currentUser._id,
+				followingId: args.followingId,
+			});
+
+			await updateFollowCounts(
+				ctx,
+				currentUser._id,
+				args.followingId,
+				"follow"
+			);
+
+			// create a notification
+			await ctx.db.insert("notifications", {
+				receiverId: args.followingId,
+				senderId: currentUser._id,
+				type: "follow",
+			});
+		}
+	},
+});
+
+const updateFollowCounts = async (
+	ctx,
+	followerId,
+	followingId,
+	whatTheyDid
+) => {
+	// purpose: update `follow count` of the person `who is following` &
+	// the person `who is being followed`
+	const follower = await ctx.db.get(followerId);
+	const following = await ctx.db.get(followingId);
+
+	await ctx.db.patch(followerId, {
+		following: follower.following + (whatTheyDid === "follow" ? 1 : -1),
+	});
+
+	await ctx.db.patch(followingId, {
+		followers: following.followers + (whatTheyDid === "follow" ? 1 : -1),
+	});
+};
